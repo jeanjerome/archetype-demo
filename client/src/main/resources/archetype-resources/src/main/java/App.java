a1a733b40dc004ab74a49848d4dd62c58a01f4c3
jeanjerome <jeanjerome.levy@gmail.com>
2020-04-13 13:19:46 +0200

    Reformat code

--- client/src/main/resources/archetype-resources/src/main/java/App.java
+++ client/src/main/resources/archetype-resources/src/main/java/App.java
@@ -2,16 +2,13 @@ package $package;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-#if( $addSecurity == "true" )
-        System.out.println( "Hello World with SAF!" );
+public class App {
+    public static void main(String[] args) {
+#if ($addSecurity == "true")
+            System.out.println("Hello World with SAF!");
 #else
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
 #end
     }
 }
--- create-app-from-archetypes.sh
+++ create-app-from-archetypes.sh
@@ -4,34 +4,33 @@ mkdir ./test-dir
 cd ./test-dir
 
 mvn archetype:generate \
--DarchetypeGroupId=com.sot.module \
--DarchetypeArtifactId=archetype-parent \
--DarchetypeVersion=RELEASE \
--DgroupId=com.example.myproject \
--DartifactId=app-parent \
--Dversion=0.0.1-SNAPSHOT \
--DinteractiveMode=false
+  -DarchetypeGroupId=com.sot.module \
+  -DarchetypeArtifactId=archetype-parent \
+  -DarchetypeVersion=RELEASE \
+  -DgroupId=com.example.myproject \
+  -DartifactId=app-parent \
+  -Dversion=0.0.1-SNAPSHOT \
+  -DinteractiveMode=false
 
 cd ./app-parent
 
 mvn archetype:generate \
--DarchetypeGroupId=com.sot.module \
--DarchetypeArtifactId=archetype-client \
--DarchetypeVersion=RELEASE \
--DgroupId=com.example.myproject \
--DartifactId=client \
--Dversion=0.0.1-SNAPSHOT \
--Dpackage=com.example.myproject.client \
--DaddSecurity=true \
--DinteractiveMode=false 
+  -DarchetypeGroupId=com.sot.module \
+  -DarchetypeArtifactId=archetype-client \
+  -DarchetypeVersion=RELEASE \
+  -DgroupId=com.example.myproject \
+  -DartifactId=client \
+  -Dversion=0.0.1-SNAPSHOT \
+  -Dpackage=com.example.myproject.client \
+  -DaddSecurity=true \
+  -DinteractiveMode=false
 
 mvn archetype:generate \
--DarchetypeGroupId=com.sot.module \
--DarchetypeArtifactId=archetype-server \
--DarchetypeVersion=RELEASE \
--DgroupId=com.example.myproject \
--DartifactId=server \
--Dversion=0.0.1-SNAPSHOT \
--Dpackage=com.example.myproject.server \
--DinteractiveMode=false
-
+  -DarchetypeGroupId=com.sot.module \
+  -DarchetypeArtifactId=archetype-server \
+  -DarchetypeVersion=RELEASE \
+  -DgroupId=com.example.myproject \
+  -DartifactId=server \
+  -Dversion=0.0.1-SNAPSHOT \
+  -Dpackage=com.example.myproject.server \
+  -DinteractiveMode=false
